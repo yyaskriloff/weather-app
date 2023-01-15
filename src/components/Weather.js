a31f7a504f804eceb5437746398e56de7b16f318
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2023-01-15 14:50:23 +0200

    finish weather card

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import '../styles.css';
-import { Card } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import moment from 'moment';
+
+const refresh = () => {
+    window.location.reload();
+}
 
 const Weather = ({ data }) => (
     <div className="main">
-        <p className="header">{data.name}</p>
-        <div>
+        <div className="top">
+            <p className="header">{data.name}</p>
+            <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+        </div>
+        <div className="flex">
             <p className="day">Day: {moment().format('dddd')}</p>
+            <p className="day">{moment().format('LL')}</p>
         </div>
 
-        <div>
+        <div className="flex">
             <p className="temp">Temprature: {data.main.temp} &deg;C</p>
+            <p className="temp">Humidity: {data.main.humidity} %</p>
+        </div>
+
+        <div className="flex">
+            <p className="sunrise-sunset">Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+            <p className="sunrise-sunset">Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
         </div>
 
     </div>
