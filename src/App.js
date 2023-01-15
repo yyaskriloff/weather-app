7b8c1866f27e0ad0aa8ae00569d6492809a1bf11
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2023-01-15 14:10:21 +0200

    getlocation and weather info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from "react";
+import Weather from './components/Weather';
 function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+          console.log(result);
+        });
+    }
+    fetchData();
+  }, [lat, long])
+
+
   return (
     <div className="App">
-      
+
     </div>
   );
 }
 
-export default App;
+export default App
