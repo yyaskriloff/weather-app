8023fdbc1911bfcf16b3d4ce2389ffe7443e3b6e
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2023-01-15 14:36:26 +0200

    add city temp and day to card with styles

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,19 +3,18 @@ import '../styles.css';
 import { Card } from 'semantic-ui-react'
 import moment from 'moment';
 
-const CardExampleCard = ({ data }) => (
-    <Card>
-        <Card.Content>
-            <Card.Header className="header">City Name: {data.name}</Card.Header>
-            <p>Temprature: {data.main.temp} &deg;C</p>
-            <p>Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Description: {data.weather[0].main}</p>
-            <p>Humidity: {data.main.humidity} %</p>
-            <p>Day: {moment().format('dddd')}</p>
-            <p>Date: {moment().format('LL')}</p>
-        </Card.Content>
-    </Card>
+const Weather = ({ data }) => (
+    <div className="main">
+        <p className="header">{data.name}</p>
+        <div>
+            <p className="day">Day: {moment().format('dddd')}</p>
+        </div>
+
+        <div>
+            <p className="temp">Temprature: {data.main.temp} &deg;C</p>
+        </div>
+
+    </div>
 )
 
-export default CardExampleCard;
+export default Weather;
